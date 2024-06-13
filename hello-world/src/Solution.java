1a8d5e523f9f75bc6cd638469c04e95a06be7c78
octoz4ms <octoz4ms@gmail.com>
2024-06-13 22:35:55 +0800

    update

--- hello-world/src/Solution.java
+++ hello-world/src/Solution.java
@@ -1,25 +1,24 @@
-import java.util.Stack;
+
 
 public class Solution {
     /**
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
      *
      *
-     * @param pushV int整型一维数组
-     * @param popV int整型一维数组
-     * @return bool布尔型
+     * @param n int整型
+     * @return int整型
      */
-    public static boolean IsPopOrder (int[] pushV, int[] popV) {
-
-        Stack<Integer> stack = new Stack<>();
-        int i = 0;
-        for (int p : pushV) {
-            stack.add(p);
-            while (!stack.isEmpty() && p == popV[i]){
-                stack.pop();
-                i++;
-            }
+    public static int findNthDigit (int n) {
+        // write code here
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i <= n; i++) {
+            builder.append(i);
         }
-        return stack.isEmpty();
+        char[] charArray = builder.toString().toCharArray();
+        return Character.getNumericValue(charArray[n]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findNthDigit(3));
     }
 }
