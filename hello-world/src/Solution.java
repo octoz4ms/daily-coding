5ddee8f06dd7702daf7414c955decd6550e134e5
octoz4ms <octoz4ms@gmail.com>
2024-12-13 17:31:08 +0800

    update

--- hello-world/src/Solution.java
+++ hello-world/src/Solution.java
@@ -1,8 +1,27 @@
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Solution {
     public static void main(String[] args) {
+        Comparator<Integer> comparator = new Comparator<>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        };
 
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(comparator);
+        priorityQueue.add(3);
+        priorityQueue.add(7);
+        priorityQueue.add(10);
+        priorityQueue.add(2);
+        System.out.println(priorityQueue);
+
+        while (!priorityQueue.isEmpty()){
+            System.out.println(priorityQueue.poll());
+        }
+
+        System.out.println(priorityQueue);
     }
 
 }
--- spring-cloud-demo/user-service/src/main/java/com/octo/cloud/us/controller/UserController.java
+++ spring-cloud-demo/user-service/src/main/java/com/octo/cloud/us/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.octo.cloud.us.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.octo.cloud.us.openfeign.OrderClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class UserController {
 
-    @Autowired
-    private OrderClient orderClient;
+    @GetMapping("/limit/{id}")
+    @SentinelResource(value = "limit")
+    public String limit(@PathVariable int id) {
+        return "用户id：" + id;
+    }
 
-    @GetMapping("/hello")
-    public void getUser() {
-        System.out.println("----------------------");
-//        String order = orderClient.getOrder();
-//        log.info("用户zms:" + order);
+
+    @GetMapping("/refuse/{id}")
+    @SentinelResource(value = "refuse")
+    public String refuse(@PathVariable int id) throws InterruptedException {
+        Thread.sleep(60);
+        return "用户id：" + id;
+    }
+
+    public String handleFallback(int id) {
+        return "熔断！";
+    }
+
+    public String handleBlockHandler(int id, BlockException ex) {
+        ex.printStackTrace();
+        return "限流！";
     }
 }
