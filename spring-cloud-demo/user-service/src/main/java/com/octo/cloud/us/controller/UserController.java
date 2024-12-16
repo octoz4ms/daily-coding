6161a51e94199d6e2d0ebffb356862af1e1b184f
octoz4ms <octoz4ms@gmail.com>
2024-12-16 17:37:25 +0800

    update

--- spring-cloud-demo/user-service/src/main/java/com/octo/cloud/us/controller/UserController.java
+++ spring-cloud-demo/user-service/src/main/java/com/octo/cloud/us/controller/UserController.java
@@ -2,9 +2,7 @@ package com.octo.cloud.us.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
-import com.octo.cloud.us.openfeign.OrderClient;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
